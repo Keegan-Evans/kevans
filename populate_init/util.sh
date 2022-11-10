2b15ea7f22e45b38661b01690febd6bc520cd58c
Keegan Evans <keegan.evans@gmail.com>
2022-11-10 15:08:38 -0700

    FEAT: add method to dev_utils that automates getting all of an objects methods.

--- populate_init/util.sh
+++ populate_init/util.sh
@@ -1,12 +1,26 @@
-function discover_python_function_definitions() {
-    FILE=$1
-    defs=${awk '/^def\ [^_][a-zA-z_0-9]*\(/' $FILE}
+function get_function_names() {
+    file=$1
+    raw_names=$(awk '
+      BEGIN { raw = "" };
+      /^def\ [^_][a-zA-z_0-9]*\(/ { raw=raw $2 " " };
+      END { print raw }' $file)
+
+      IFS=", " read -ra raw_array <<< "$raw_names"
+
+      for i in "${raw_array[@]}"
+      do
+          IFS="(" read -ra cleaned_name <<< "$i"
+          printf "$cleaned_name\n"
+      done
 }
 
+function parse_init(){
+    current_init=$1
+}
 
 if [ $1 ]
 then
     provided=$1
-    printf "${provided}\n"
-    discover_python_function_definitions provided
+    get_function_names $provided
 fi
+
